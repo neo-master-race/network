6ff4757dfce7a1f59aedbabec76dc56ec60589e0
ludovicm67 <ludovicmuller1@gmail.com>
2018-02-14 16:47:08 +0100

    new project: `mix new network --sup`

---
+++ mix.exs
@@ -0,0 +1,26 @@
+defmodule Network.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :network,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Network.Application, []}
+    ]
+  end
+
+  # Les dépendences
+  defp deps do
+    [
+    ]
+  end
+end
