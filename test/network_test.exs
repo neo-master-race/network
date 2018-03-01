7b3a40a5f96646bfef406b4b63e485ceb2c08c15
ludovicm67 <ludovicmuller1@gmail.com>
2018-03-01 17:31:40 +0100

    removed tests for the moment

--- test/network_test.exs
+++ test/network_test.exs
@@ -2,7 +2,7 @@ defmodule NetworkTest do
   use ExUnit.Case
   doctest Network
 
-  test "greets the world" do
-    assert Network.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Network.hello() == :world
+  # end
 end
