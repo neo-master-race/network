42be1accdbcbfd03a2e2b7991a48b71f0ee67c38
ludovicm67 <ludovicmuller1@gmail.com>
2018-03-01 17:29:59 +0100

    close #3 : can now manage TCP sockets

--- lib/network.ex
+++ lib/network.ex
@@ -1,18 +1,64 @@
+require Logger
+
 defmodule Network do
   @moduledoc """
-  Documentation for Network.
+  Manage TCP sockets
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Network.hello
-      :world
-
+  Initializing the network part (listening on a specific port)
   """
-  def hello do
-    :world
+  def accept(port) do
+    {:ok, clients} = Agent.start_link(fn -> [] end)
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+
+    Logger.info("accepting connections on port #{port}")
+    loop_acceptor(socket, clients)
+  end
+
+  defp loop_acceptor(socket, clients) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    clientId = Port.info(client)[:id]
+    Logger.info("client #{clientId} joined!")
+    write_info(client, "successfully connected to the server!")
+    Agent.update(clients, fn list -> [client] ++ list end)
+
+    {:ok, pid} = Task.Supervisor.start_child(Network.TaskSupervisor, fn -> serve(client, clients) end)
+
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket, clients)
+  end
+
+  defp serve(socket, clients) do
+    clientId = Port.info(socket)[:id]
+    case read_line(socket) do
+      {:ok, data} ->
+        Logger.info("client #{clientId} sent message: #{data}")
+        Agent.get(clients, fn list ->
+          Enum.each(list, fn c -> write_line(c, data) end)
+        end)
+
+        serve(socket, clients)
+      {:logout, reason} ->
+        Agent.update(clients, fn list -> list -- [socket] end)
+        Logger.info("client #{clientId} has leaved. Reason: #{reason}")
+      {:error, msg} -> Logger.error(msg)
+    end
+  end
+
+  defp read_line(socket) do
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, data} -> {:ok, data}
+      {:error, :closed} -> {:logout, "socket is closed"}
+      _ -> {:error, "unknown error"}
+    end
+  end
+
+  defp write_line(socket, line) do
+    :gen_tcp.send(socket, line)
+  end
+
+  defp write_info(socket, line) do
+    write_line(socket, "[INFO] #{line}\n")
   end
 end
--- lib/network/application.ex
+++ lib/network/application.ex
@@ -1,19 +1,14 @@
 defmodule Network.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: Network.Worker.start_link(arg)
-      # {Network.Worker, arg},
+      {Task.Supervisor, name: Network.TaskSupervisor},
+      {Task, fn -> Network.accept(4242) end}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Network.Supervisor]
     Supervisor.start_link(children, opts)
   end
