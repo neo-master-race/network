519e0d4e1d7cace59b8a281a094a309b547cbd66
caswitch <mariefrance.kom@gmail.com>
2018-03-21 14:25:54 +0100

    Create room

--- lib/network/room.ex
+++ lib/network/room.ex
@@ -1,6 +1,6 @@
 defmodule Network.Room do
   @moduledoc """
-  Describe the state of a room
+  Describe the state of a room.
   """
 
   use GenServer
@@ -14,8 +14,23 @@ defmodule Network.Room do
       # right on the room's modif
       creator: id_creator,
       players: %{},
-      started: false,
-      position: nil
+      started: false
     }
+  end
+
+  def start_link(_args) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(default) do
+    {:ok, default}
+  end
+
+  def start(id) do
+    GenServer.call(__MODULE__, :start)
+  end
+
+  def handle_call({:start, id}, _from, state) do
+    {:reply, :ok, %{state | started: true}}
   end
 end
